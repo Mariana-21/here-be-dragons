964ca8b7e22b67adcddb3915bca9f0cb554daab1
Mariana Cid <cidmariana5@gmail.com>
2020-06-27 16:40:56 -0600

    Passing tests

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -12,6 +12,8 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
+    # binding.pry
+    result = @hash[:results]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
@@ -19,13 +21,18 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
 
+    result = @hash[:results]
+
     assert result.is_a? (Array)
     assert_equal 44, result.count
   end
 
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
+    data = @hash[:results]
+    result = data.find_all {|res| res[:subsection] == "Politics"}
 
+      # binding.pry
 
     assert result.is_a? (Array)
     assert_equal 6, result.count
